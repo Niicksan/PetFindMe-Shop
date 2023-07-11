53a89ebc3b4ab2e5ad66549f923986f75cde7fae
Niicksan <nikslotar@abv.bg>
2023-07-11 23:57:07 +0300

    PetFindMe Shop setup layout

---
+++ PetFindMeShop.Data/PetFindMeShopDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace PetFindMeShop.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class PetFindMeShopDbContext : IdentityDbContext
+    {
+        public PetFindMeShopDbContext(DbContextOptions<PetFindMeShopDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ PetFindMeShop.ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PetFindMeShop.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
