f1f3993ce88c448ffca26f476e62015e8f5b3b5d
Niicksan <nikslotar@abv.bg>
2023-08-14 18:48:53 +0300

    Add Configuration for ShoppingCart Entity

---
+++ PetFindMeShop.Data/Configurations/ShoppingCartEntityConfiguration.cs
@@ -0,0 +1,24 @@
+﻿namespace PetFindMeShop.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using PetFindMeShop.Data.Models;
+
+    public class ShoppingCartEntityConfiguration : IEntityTypeConfiguration<ShoppingCart>
+    {
+        public void Configure(EntityTypeBuilder<ShoppingCart> builder)
+        {
+            builder
+             .Property(sc => sc.TotalProductsPrice)
+             .HasDefaultValueSql(null);
+
+            builder
+              .Property(sc => sc.CreatedAt)
+              .HasDefaultValueSql("GETDATE()");
+
+            builder
+               .Property(sc => sc.UpdatedAt)
+               .HasDefaultValueSql("GETDATE()");
+        }
+    }
+}
